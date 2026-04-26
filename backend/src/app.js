2b3c24c0ede005fb3b4538320d9b09c6f343059e
Daniel Echeverría <d.echever23@gmail.com>
2026-04-26 18:18:31 -0500

    feat(setup): add initial app and server configuration with frontend global styles

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,23 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    }));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "TactiCup 26 API running" });
+    res.json({
+    message: "TactiCup 26 API running",
+    status: "ok",
+    });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+export default app;
